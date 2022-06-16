dc2e2e20225ef72e6e06db02f94b9edbe0a1bc4e
hcduffey <cliff.duffey@pm.me>
2022-06-16 10:05:34 -0400

    test captcha

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -1,6 +1,24 @@
+import { ReCAPTCHA } from "react-google-recaptcha";
+
+const recaptchaRef = React.createRef();
+
 const ContactForm = (props) => {
+    const onSubmit = () => {
+        const recaptchaValue = recaptchaRef.current.getValue();
+        this.props.onSubmit(recaptchaValue);
+    }
+    
+    const onChange = (value) => {
+        console.log("Captcha value:", value);
+    }
+    
     return(
-        <form action="mailto:cliff.duffey@pm.me" method="GET">
+        <form onSubmit={onSubmit} method="POST">
+            <ReCAPTCHA
+                ref={recaptchaRef}
+                sitekey="6LeNaXYgAAAAAGkpBuXg241nNmporHJZbq2ilNVn"
+                onChange={onChange}
+            />
             <label htmlFor="subject">Subject</label>
             <input name="subject" type="text" />
             <label htmlFor="message">Message</label>
